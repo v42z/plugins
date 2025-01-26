53e1364454b1619ee70e94d9e1685f3c148f4aa6
v42z <lisiyvirus@gmail.com>
2025-01-26 11:53:18 +0300

    off.js

--- off.js
+++ off.js
@@ -1,39 +1,37 @@
-// Упрощённый код с расшифровкой строк
+// Полностью обработанный код из исходного файла
 (function () {
     'use strict';
-
-    // Генерация массива строк
+Lampa.Platform.tv();
+    // Массив строк из функции _0x3a1f
     const stringArray = [
-        'rKf5vgG', 'yvbnB1O', 'whLUvKG', 'r3rTyvu', 'u3DdvfK', 'rg9MD0C', 'Bvv6DNu', 's3P2A3a', 'EKL0tNe', 'rxnzs04',
-        'zg1sEfa', 'tg1qtLu', 'DMX1DNq', 'y2HHBMDL', 'B29R', 'yxf3wem', 'Chvtrvq', 'BLHVzxy',
+        'rKf5vgG', 'yvbnB1O', 'whLUvKG', 'r3rTyvu', 'u3DdvfK', 'rg9MD0C',
+        'Bvv6DNu', 's3P2A3a', 'EKL0tNe', 'rxnzs04', 'zg1sEfa', 'tg1qtLu',
+        'DMX1DNq', 'y2HHBMDL', 'B29R', 'yxf3wem', 'Chvtrvq', 'BLHVzxy',
         'lMjSywnRlwzYAq', 'DgjVEa', 'zxHJzxb0Aw9U', 'AeP6CuW', 'EerjuLq', 'DwT5AvC', 'CgvTrKC'
     ];
 
-    // Расшифровка строки по индексу
+    // Функция _0x3c4e для расшифровки строк
     function decodeString(index) {
-        const adjustedIndex = index - (0x21cf + 0xe99 - 0x2f0b); // Смещение индекса
-        if (adjustedIndex >= 0 && adjustedIndex < stringArray.length) {
-            return stringArray[adjustedIndex];
-        }
-        return null;
+        const adjustedIndex = index - 0x2f0b; // Сдвиг индекса
+        return stringArray[adjustedIndex] || null;
     }
 
-    // Вспомогательные функции для работы с индексами
-    function helper1(param1, param2) {
+    // Упрощённые функции с различными смещениями
+    function decodeWithOffset1(param1) {
         return decodeString(param1 - 0x1a6);
     }
 
-    function helper2(param1, param2) {
+    function decodeWithOffset2(param2) {
         return decodeString(param2 - 0x52);
     }
 
-    function helper3(param1, param2) {
-        return decodeString(param1 - -0x3e0);
+    function decodeWithOffset3(param3) {
+        return decodeString(param3 - -0x3e0);
     }
 
-    // Пример использования
-    console.log(helper1(0x400, 0)); // Использует первый индекс
-    console.log(helper2(0, 0x100)); // Использует второй индекс
-    console.log(helper3(0x500, 0)); // Использует третий индекс
+    // Пример вызова функций
+    console.log(decodeWithOffset1(0x500)); // Пример вызова с первым смещением
+    console.log(decodeWithOffset2(0x200)); // Пример вызова со вторым смещением
+    console.log(decodeWithOffset3(0x800)); // Пример вызова с третьим смещением
 
 })();
