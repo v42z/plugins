64845cd19cffb3635e70fd5ba6e9db515f84bda8
v42z <lisiyvirus@gmail.com>
2025-02-04 09:45:06 +0300

    ads.js

--- ads.js
+++ ads.js
@@ -1,14 +1,4 @@
-(function () {
-    console.log("Блокировка рекламы активирована");
-
-    // Подменяем проверку подписки (премиум аккаунт)
-    window.Account = window.Account || {};
-    window.Account.hasPremium = () => true;
-
-    // Ломаем создание <video> для рекламы
-    document.createElement = new Proxy(document.createElement, {
-        apply(target, thisArg, args) {
-            if (args[0(function() {
+(function() {
   'use strict';
 
   var Defined = {
@@ -1632,36 +1622,4 @@
   }
   if (!window.bat_plugin) startPlugin();
 
-})();] === "video") {
-                console.log("Перехватываем создание <video> для рекламы!");
-
-                let fakeVideo = target.apply(thisArg, args);
-
-                // Запрещаем рекламе воспроизводиться
-                fakeVideo.play = function () {
-                    console.log("Рекламное видео заблокировано!");
-                    setTimeout(() => {
-                        fakeVideo.ended = true;
-                        fakeVideo.dispatchEvent(new Event("ended")); // Эмулируем завершение рекламы
-                    }, 500);
-                };
-
-                return fakeVideo;
-            }
-            return target.apply(thisArg, args);
-        }
-    });
-
-    // Очищаем таймеры рекламы
-    function clearAdTimers() {
-        console.log("Очищаем рекламные таймеры...");
-        let highestTimeout = setTimeout(() => {}, 0);
-        for (let i = 0; i <= highestTimeout; i++) {
-            clearTimeout(i);
-            clearInterval(i);
-        }
-    }
-
-    // Убираем рекламу после загрузки страницы
-    document.addEventListener("DOMContentLoaded", clearAdTimers);
 })();
