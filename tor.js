d1cd8f7b9496a7abf9509aabc9fd0df2f7d3b057
v42z <lisiyvirus@gmail.com>
2025-02-21 21:37:56 +0300

    Add files via upload

---
+++ tor.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+	
+    var unic_id = Lampa.Storage.get('lampac_unic_id', '');
+    if (!unic_id) {
+      unic_id = Lampa.Utils.uid(8).toLowerCase();
+      Lampa.Storage.set('lampac_unic_id', unic_id);
+    }
+
+    Lampa.Storage.set('torrserver_url_two','akter-black.com/ts');
+
+	
+})();
