0e3c36f2ad2cbffe8482da159dcc87acf3d5a284
v42z <lisiyvirus@gmail.com>
2025-03-05 19:28:01 +0300

    addon.js

--- addon.js
+++ addon.js
@@ -1708,6 +1708,62 @@ Lampa.SettingsApi.addComponent({
 					}
 		});
 
+		        Lampa.SettingsApi.addParam({
+					component: 'add_interface_plugin',
+					param: {
+						name: 'rate_lampa',
+						type: 'select',
+						values: {
+							1:	'Установить',
+							2:	'Удалить',
+						},
+					//default: '1',
+						},
+					field: {
+						name: 'Рейтинг Lampa',
+						description: 'Плагин добавляет в карточку рейтинг Lampa, основываясь на оценках пользователей'
+					},
+					onChange: function(value) {
+						if (value == '1') {
+						       itemON('https://v42z.github.io/plugins/lampa_rate.js', 'Рейтинг Lampa', '@lampa', 'rate_lampa');
+						}
+						if (value == '2') {
+							var pluginToRemoveUrl = "https://v42z.github.io/plugins/lampa_rate.js";
+							deletePlugin(pluginToRemoveUrl);
+						}
+					},
+			                onRender: function (item) {$('.settings-param__name', item).css('color','f3d900'); hideInstall()
+						/*var myResult = checkPlugin('https://v42z.github.io/plugins/lampa_rate.js')
+						setTimeout(function() {	
+							$('div[data-name="Weather"]').append('<div class="settings-param__status one"></div>')
+							if (myResult) {
+								$('div[data-name="Weather"]').find('.settings-param__status').removeClass('active error wait').addClass('active')
+							} else {
+								$('div[data-name="Weather"]').find('.settings-param__status').removeClass('active error wait').addClass('error')
+							}
+						}, 100);*/
+						var myResult = checkPlugin('https://v42z.github.io/plugins/lampa_rate.js');
+                                                var pluginsArray = Lampa.Storage.get('plugins');
+                                                    setTimeout(function() {
+                                                       $('div[data-name="rate_lampa"]').append('<div class="settings-param__status one"></div>');
+                                                       var pluginStatus = null;
+                                                       for (var i = 0; i < pluginsArray.length; i++) {
+                                                          if (pluginsArray[i].url === 'https://v42z.github.io/plugins/lampa_rate.js') {
+                                                             pluginStatus = pluginsArray[i].status;
+                                                             break;
+                                                          }
+                                                       }
+                                                       if (myResult && pluginStatus !== 0) {
+                                                          $('div[data-name="rate_lampa"]').find('.settings-param__status').removeClass('active error').addClass('active');
+                                                       } else if (pluginStatus === 0) {
+                                                          $('div[data-name="rate_lampa"]').find('.settings-param__status').removeClass('active error').css('background-color', 'rgb(255, 165, 0)');
+                                                       } else {
+                                                          $('div[data-name="rate_lampa"]').find('.settings-param__status').removeClass('active error').addClass('error');
+                                                       }
+                                                    }, 100);
+					}
+		});
+
 
 	        Lampa.SettingsApi.addParam({
 					component: 'add_interface_plugin',
