e1181afc724439ae369d6fd58992c0db1c9e54c7
v42z <lisiyvirus@gmail.com>
2025-03-23 20:33:30 +0300

    addon.js

--- addon.js
+++ addon.js
@@ -1425,7 +1425,61 @@ Lampa.SettingsApi.addComponent({
 					}
 		});
 
-
+	        Lampa.SettingsApi.addParam({
+					component: 'add_interface_plugin',
+					param: {
+						name: 'in_qual',
+						type: 'select',
+						values: {
+							1:	'Установить',
+							2:	'Удалить',
+						},
+					//default: '1',
+						},
+					field: {
+						name: 'В качестве',
+						description: 'Плагин добавляет в левом меню пункт с фильмами, которые вышли в высоком качестве'
+					},
+					onChange: function(value) {
+						if (value == '1') {
+						       itemON('https://v42z.github.io/plugins/quality.js', 'В Качестве', '@lampa', 'qual');
+						}
+						if (value == '2') {
+							var pluginToRemoveUrl = "https://v42z.github.io/plugins/quality.js";
+							deletePlugin(pluginToRemoveUrl);
+						}
+					},
+			                onRender: function (item) {$('.settings-param__name', item).css('color','f3d900'); hideInstall()
+						/*var myResult = checkPlugin('https://v42z.github.io/plugins/weather.js')
+						setTimeout(function() {	
+							$('div[data-name="Weather"]').append('<div class="settings-param__status one"></div>')
+							if (myResult) {
+								$('div[data-name="Weather"]').find('.settings-param__status').removeClass('active error wait').addClass('active')
+							} else {
+								$('div[data-name="Weather"]').find('.settings-param__status').removeClass('active error wait').addClass('error')
+							}
+						}, 100);*/
+						var myResult = checkPlugin('https://v42z.github.io/plugins/quality.js');
+                                                var pluginsArray = Lampa.Storage.get('plugins');
+                                                    setTimeout(function() {
+                                                       $('div[data-name="in_qual"]').append('<div class="settings-param__status one"></div>');
+                                                       var pluginStatus = null;
+                                                       for (var i = 0; i < pluginsArray.length; i++) {
+                                                          if (pluginsArray[i].url === 'https://v42z.github.io/plugins/quality.js') {
+                                                             pluginStatus = pluginsArray[i].status;
+                                                             break;
+                                                          }
+                                                       }
+                                                       if (myResult && pluginStatus !== 0) {
+                                                          $('div[data-name="in_qual"]').find('.settings-param__status').removeClass('active error').addClass('active');
+                                                       } else if (pluginStatus === 0) {
+                                                          $('div[data-name="in_qual"]').find('.settings-param__status').removeClass('active error').css('background-color', 'rgb(255, 165, 0)');
+                                                       } else {
+                                                          $('div[data-name="in_qual"]').find('.settings-param__status').removeClass('active error').addClass('error');
+                                                       }
+                                                    }, 100);
+					}
+		});
 
 	        Lampa.SettingsApi.addParam({
 					component: 'add_interface_plugin',
