22e3eae6872eb5b615535804ada8e423e1016230
v42z <lisiyvirus@gmail.com>
2025-08-10 16:24:55 +0300

    addon.js

--- addon.js
+++ addon.js
@@ -2751,6 +2751,62 @@ Lampa.SettingsApi.addComponent({
 						});
 					}
 		});
+
+	   		Lampa.SettingsApi.addParam({
+					component: 'add_online_plugin',
+					param: {
+                                		name: 'Smotret24',
+						type: 'select',
+						values: {
+							1:	'Установить',
+							2:	'Удалить',
+					},
+					//default: '1',
+              				},
+					field: {
+						name: 'Smotret24',
+						description: 'Плагин для онлайн Смотреть24'
+					},
+					onChange: function(value) {
+						if (value == '1') {
+							itemON('http://smotret24.ru/online.js', 'Smotret24', '@lampa', 'Smotret24');
+						}
+						if (value == '2') {
+							var pluginToRemoveUrl = "http://smotret24.ru/online.js";
+							deletePlugin(pluginToRemoveUrl);
+						}
+					},
+					onRender: function (item) {$('.settings-param__name', item).css('color','f3d900'); hideInstall();
+						/*var myResult = checkPlugin('http://smotret24.ru/online.js')
+						setTimeout(function() {	
+							$('div[data-name="Smotret24"]').append('<div class="settings-param__status one"></div>')
+							if (myResult) {
+								$('div[data-name="Smotret24"]').find('.settings-param__status').removeClass('active error wait').addClass('active')
+							} else {
+								$('div[data-name="Smotret24"]').find('.settings-param__status').removeClass('active error wait').addClass('error')
+							}
+						}, 100);*/
+						var myResult = checkPlugin('http://smotret24.ru/online.js');
+                                                var pluginsArray = Lampa.Storage.get('plugins');
+                                                    setTimeout(function() {
+                                                       $('div[data-name="Smotret24"]').append('<div class="settings-param__status one"></div>');
+                                                       var pluginStatus = null;
+                                                       for (var i = 0; i < pluginsArray.length; i++) {
+                                                          if (pluginsArray[i].url === 'http://smotret24.ru/online.js') {
+                                                             pluginStatus = pluginsArray[i].status;
+                                                             break;
+                                                          }
+                                                       }
+                                                       if (myResult && pluginStatus !== 0) {
+                                                          $('div[data-name="Smotret24"]').find('.settings-param__status').removeClass('active error').addClass('active');
+                                                       } else if (pluginStatus === 0) {
+                                                          $('div[data-name="Smotret24"]').find('.settings-param__status').removeClass('active error').css('background-color', 'rgb(255, 165, 0)');
+                                                       } else {
+                                                          $('div[data-name="Smotret24"]').find('.settings-param__status').removeClass('active error').addClass('error');
+                                                       }
+                                                    }, 100);
+					}
+        });
                  
 		Lampa.SettingsApi.addParam({
 					component: 'add_online_plugin',
