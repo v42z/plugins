dfabf945efaafb7e49cb1d484d042a5ac5116d11
v42z <lisiyvirus@gmail.com>
2025-08-30 21:33:15 +0300

    addon.js

--- addon.js
+++ addon.js
@@ -683,62 +683,6 @@ Lampa.SettingsApi.addComponent({
                                                     }, 100);
 					}
 		});
-       
-		Lampa.SettingsApi.addParam({
-					component: 'add_interface_plugin',
-					param: {
-						name: 'Tricks',
-						type: 'select',
-						values: {
-							1:	'Установить',
-							2:	'Удалить',
-						},
-					//default: '1',
-						},
-					field: {
-						name: 'Приятные мелочи',
-						description: 'Плагин позволяет на выбор установить различные дополнения (скринсейверы, стилизация кнопок, стиль плеера, часы в плеере и т.п.)'
-					},
-					onChange: function(value) {
-						if (value == '1') {
-							itemON('https://amikdn.github.io/tricks.js', 'Приятные Мелочи', '@lampa', 'Tricks');
-						}
-						if (value == '2') {
-							var pluginToRemoveUrl = "https://amikdn.github.io/tricks.js";
-							deletePlugin(pluginToRemoveUrl);
-						}	
-					},
-					onRender: function (item) {$('.settings-param__name', item).css('color','f3d900');  hideInstall()
-						/*var myResult = checkPlugin('https://amikdn.github.io/tricks.js')
-						setTimeout(function() {	
-							$('div[data-name="Tricks"]').append('<div class="settings-param__status one"></div>')
-							if (myResult) {
-								$('div[data-name="Tricks"]').find('.settings-param__status').removeClass('active error wait').addClass('active')
-							} else {
-								$('div[data-name="Tricks"]').find('.settings-param__status').removeClass('active error wait').addClass('error')
-							}
-						}, 100);*/
-						var myResult = checkPlugin('https://amikdn.github.io/tricks.js');
-                                                var pluginsArray = Lampa.Storage.get('plugins');
-                                                    setTimeout(function() {
-                                                       $('div[data-name="Tricks"]').append('<div class="settings-param__status one"></div>');
-                                                       var pluginStatus = null;
-                                                       for (var i = 0; i < pluginsArray.length; i++) {
-                                                          if (pluginsArray[i].url === 'https://amikdn.github.io/tricks.js') {
-                                                             pluginStatus = pluginsArray[i].status;
-                                                             break;
-                                                          }
-                                                       }
-                                                       if (myResult && pluginStatus !== 0) {
-                                                          $('div[data-name="Tricks"]').find('.settings-param__status').removeClass('active error').addClass('active');
-                                                       } else if (pluginStatus === 0) {
-                                                          $('div[data-name="Tricks"]').find('.settings-param__status').removeClass('active error').css('background-color', 'rgb(255, 165, 0)');
-                                                       } else {
-                                                          $('div[data-name="Tricks"]').find('.settings-param__status').removeClass('active error').addClass('error');
-                                                       }
-                                                    }, 100);
-					}
-		});
 		
 		Lampa.SettingsApi.addParam({
 					component: 'add_interface_plugin',
