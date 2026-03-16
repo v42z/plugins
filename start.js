11ab8c823118aaf701167d2b9f9944d4da3ac562
v42z <lisiyvirus@gmail.com>
2026-03-17 02:41:16 +0300

    Update start.js

--- start.js
+++ start.js
@@ -1,62 +1,128 @@
 (function () {
     'use strict';
-    Lampa.Platform.tv();
-    var plugin = {
-        name: 'TMDB Proxy with Anti-DMCA',
-        version: '1.0.3',
-        description: 'Проксирование постеров и API сайта TMDB с отключением DMCA-фич и обходом блокировок'
-    };
-    plugin.path_image = Lampa.Utils.protocol() + 'tmdbimage.abmsx.tech/';  //  tmdbimg.bylampa.online/  tmdbimage.abmsx.tech/
-    plugin.path_api = Lampa.Utils.protocol() + 'tmdb.abmsx.tech/3/'; //  tmdbapi.bylampa.online/3/   tmdb.abmsx.tech/3/
 
-    Lampa.TMDB.image = function (url) {
-        var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
-        return Lampa.Storage.field('proxy_tmdb') ? plugin.path_image + url : base;
-    };
+    var tmdbProxyHost = 'apitmdb.cub.rip';
+    var tmdbDirectHost = 'api.themoviedb.org';
+    var apiKey = '4ef0d7355d9ffb5151e987764708ce96';
 
-    Lampa.TMDB.api = function (url) {
-        var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
-        return Lampa.Storage.field('proxy_tmdb') ? plugin.path_api + url : base;
-    };
+    function fixUrl(url) {
+        if (typeof url !== 'string') return url;
+        if (url.indexOf(tmdbProxyHost) !== -1) url = url.replace(tmdbProxyHost, tmdbDirectHost);
+        return url;
+    }
 
-    window.lampa_settings = window.lampa_settings || {};
-    window.lampa_settings.dcma = false;
-    window.lampa_settings.disable_features = window.lampa_settings.disable_features || {};
-    window.lampa_settings.disable_features.dmca = true;
+    (function patchNetwork() {
+        var origOpen = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function () {
+            var args = Array.prototype.slice.call(arguments);
+            if (typeof args[1] === 'string') args[1] = fixUrl(args[1]);
+            return origOpen.apply(this, args);
+        };
+        if (typeof fetch !== 'undefined') {
+            var origFetch = window.fetch;
+            window.fetch = function (url, opts) {
+                if (typeof url === 'string') url = fixUrl(url);
+                return origFetch.call(this, url, opts);
+            };
+        }
+    })();
 
     function start() {
-        if (window.anti_dmca_plugin) {
-            return;
-        }
+        if (window.anti_dmca_plugin) return;
+        if (typeof Lampa === 'undefined' || !window.lampa_settings) return;
+
         window.anti_dmca_plugin = true;
-        Lampa.Utils.dcma = function () {
-            return undefined;
-        };
-        var defaultSource = Lampa.Storage.get('source', 'cub');
-        Lampa.Listener.follow('request_secuses', function (event) {
-            if (event.data.blocked) {
-                window.lampa_settings.dcma = [];
-                var active = Lampa.Activity.active();
-                active.source = 'tmdb';
-                Lampa.Storage.set('source', 'tmdb', true);
-                Lampa.Activity.replace(active);
-                Lampa.Storage.set('source', defaultSource, true);
+
+        Lampa.Utils.dcma = function () { return undefined };
+        try {
+            Object.defineProperty(window.lampa_settings, 'dcma', {
+                get: function () { return []; },
+                set: function () {},
+                configurable: true
+            });
+        } catch (e) {
+            window.lampa_settings.dcma = [];
+        }
+
+        var tmdbSource = Lampa.Api && Lampa.Api.sources && Lampa.Api.sources.tmdb;
+        if (tmdbSource && typeof tmdbSource.parseCountries === 'function') {
+            var origPC = tmdbSource.parseCountries;
+            tmdbSource.parseCountries = function (movie) {
+                var r = origPC.apply(this, arguments);
+                return Array.isArray(r) ? r : [];
+            };
+        }
+
+        if (window.jQuery && window.jQuery.ajax) {
+            var origAjax = window.jQuery.ajax;
+
+            function fetchFromTmdb(cardId, cardType, lang, origSuccess, origError, self, args) {
+                var tmdbUrl = 'https://' + tmdbDirectHost + '/3/' + cardType + '/' + cardId
+                    + '?api_key=' + apiKey + '&language=' + (lang || 'ru')
+                    + '&append_to_response=credits,external_ids,videos,recommendations,similar';
+                origAjax.call(window.jQuery, {
+                    url: tmdbUrl,
+                    dataType: 'json',
+                    success: function (realData) {
+                        origSuccess.call(self, realData, args[1], args[2]);
+                    },
+                    error: function () {
+                        if (origError) origError();
+                    }
+                });
             }
-        });
-        Lampa.Settings.listener.follow('open', function (e) {
-            if (e.name === 'tmdb') {
-                e.body.find('[data-parent="proxy"]').remove();
+
+            function getCardInfo() {
+                try {
+                    var active = Lampa.Activity.active();
+                    if (!active) return null;
+                    var id = active.id || (active.item && active.item.id);
+                    var type = null;
+                    if (active.method === 'tv' || active.card_type === 'tv' || (active.item && active.item.name && !active.item.title)) type = 'tv';
+                    else if (active.method === 'movie' || active.card_type === 'movie' || (active.item && active.item.title)) type = 'movie';
+                    if (id && type) return { id: id, type: type };
+                } catch (e) {}
+                return null;
             }
-        });
+
+            window.jQuery.ajax = function (urlOrSettings, options) {
+                var s = typeof urlOrSettings === 'object' && urlOrSettings !== null
+                    ? Object.assign({}, urlOrSettings)
+                    : (options ? Object.assign({ url: urlOrSettings }, options) : { url: urlOrSettings });
+
+                if (s.url && typeof s.url === 'string') {
+                    s.url = fixUrl(s.url);
+                }
+
+                if (typeof s.success === 'function') {
+                    var origSuccess = s.success;
+                    var origError = s.error;
+                    s.success = function (data) {
+                        var isObj = data && typeof data === 'object' && !Array.isArray(data);
+                        var isBlocked = isObj && data.blocked;
+                        var isEmpty = isObj && !data.blocked && !data.id && !data.title && !data.name && !data.results && Object.keys(data).length < 3;
+
+                        if (isBlocked || isEmpty) {
+                            var card = getCardInfo();
+                            if (card) {
+                                var lang = Lampa.Storage.get('tmdb_lang', 'ru');
+                                fetchFromTmdb(card.id, card.type, lang, origSuccess, origError, this, arguments);
+                                return;
+                            }
+                        }
+                        return origSuccess.apply(this, arguments);
+                    };
+                }
+                return origAjax.call(this, s);
+            };
+        }
     }
 
     if (window.appready) {
         start();
-    } else {
+    } else if (typeof Lampa !== 'undefined' && Lampa.Listener) {
         Lampa.Listener.follow('app', function (event) {
-            if (event.type === 'ready') {
-                start();
-            }
+            if (event.type === 'ready') start();
         });
     }
 })();
