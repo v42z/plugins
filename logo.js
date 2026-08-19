907ef4e6e3d7b789e9e691477214003540968a74
v42z <lisiyvirus@gmail.com>
2026-08-19 10:51:52 +0300

    Update logo.js

--- logo.js
+++ logo.js
@@ -1,62 +1,100 @@
 (function () {
     'use strict';
 
-    // Функция для запуска плагина
-    function startPlugin() {
-        // Устанавливаем флаг, что плагин активирован
-        window.logoplugin = true;
-
-        // Подписываемся на событие 'full' в Lampa.Listener
-        Lampa.Listener.follow('full', function (e) {
-            // Проверяем, что событие имеет тип 'complite' и параметр logo_glav не равен '1'
-            if (e.type === 'complite' && Lampa.Storage.get('logo_glav') !== '1') {
-                var data = e.data.movie;
-                var type = data.name ? 'tv' : 'movie';
-
-                // Проверяем, что ID фильма не пустое
-                if (data.id !== '') {
-                    // Формируем URL для запроса к API TMDB
-                    var url = Lampa.TMDB.api(type + '/' + data.id + '/images?api_key=' + Lampa.TMDB.key() + '&language=' + Lampa.Storage.get('language'));
-
-                    // Выполняем GET-запрос к API
-                    $.get(url, function (data) {
-                        // Проверяем наличие логотипов в ответе
-                        if (data.logos && data.logos[0]) {
-                            var logo = data.logos[0].file_path;
-
-                            // Если логотип существует, заменяем текстовое название на изображение
-                            if (logo !== '') {
-                                e.object.activity.render()
-                                    .find('.full-start-new__title')
-                                    .html('<img style="margin-top:5px; max-height:125px;" src="' + Lampa.TMDB.image('/t/p/w300' + logo.replace('.svg', '.png')) + '"/>');
-                            }
-                        }
-                    });
-                }
-            }
-        });
-    }
-
-    // Добавляем параметр в настройки Lampa
     Lampa.SettingsApi.addParam({
-        component: 'interface',
-        param: {
-            name: 'logo_glav',
-            type: 'select',
-            values: {
-                '1': 'Скрыть',
-                '0': 'Отображать'
-            },
-            default: '0'
-        },
-        field: {
-            name: 'Логотипы вместо названий',
-            description: 'Отображает логотипы фильмов вместо текста'
+        component: "interface",
+        param: { name: "logo_glav", type: "select", values: { 1: "Скрыть", 0: "Отображать" }, default: "0" },
+        field: { name: "Логотипы вместо названий", description: "Отображает логотипы фильмов вместо текста" }
+    });
+
+    Lampa.SettingsApi.addParam({
+        component: "interface",
+        param: { name: "logo_size", type: "select", values: { w300: "w300", w500: "w500", w780: "w780", original: "Оригинал" }, default: "w500" },
+        field: { name: "Размер логотипа", description: "Разрешение загружаемого изображения" }
+    });
+
+    Lampa.SettingsApi.addParam({
+        component: "interface",
+        param: { name: "logo_hide_year", type: "trigger", default: true },
+        field: { name: "Скрывать год и страну над логотипом", description: "Переносит год выпуска и страну под логотип" }
+    });
+
+    if (window.logoplugin) return;
+    window.logoplugin = true;
+
+    Lampa.Storage.listener.follow('change', function (e) {
+        if (['logo_glav', 'logo_size', 'logo_hide_year'].includes(e.param)) {
+            var activity = Lampa.Activity.active();
+            if (activity && activity.component === 'full') {
+                setTimeout(function () {
+                    activity.reload();
+                }, 300);
+            }
         }
     });
 
-    // Запускаем плагин, если он еще не активирован
-    if (!window.logoplugin) {
-        startPlugin();
-    }
+    Lampa.Listener.follow('full', function (a) {
+        if (a.type == 'complite' && "1" != Lampa.Storage.get("logo_glav")) {
+            var movie = a.data.movie;
+            var type = movie.name ? 'tv' : 'movie';
+            var render = a.object.activity.render();
+
+            var title = render.find(".full-start-new__title");
+            var head = render.find(".full-start-new__head");
+            var details = render.find(".full-start-new__details");
+            var tagline = render.find(".full-start-new__tagline");
+
+            if (movie.id == '') return;
+
+            var lang = Lampa.Storage.get("language");
+            var size = Lampa.Storage.get("logo_size", "w500");
+
+            var TMDB_API = "http://apitmdb.cubnotrip.top/3";
+            var url = TMDB_API + "/" + type + "/" + movie.id +"/images?api_key=" + Lampa.TMDB.key() +"&include_image_language=" + lang + ",en,null";
+
+            $.get(url, function (response) {
+                var logo_path = null;
+
+                if (response.logos && response.logos.length > 0) {
+                    for (var i = 0; i < response.logos.length; i++) {
+                        if (response.logos[i].iso_639_1 == lang) {
+                            logo_path = response.logos[i].file_path;
+                            break;
+                        }
+                    }
+                    if (!logo_path) {
+                        for (var i = 0; i < response.logos.length; i++) {
+                            if (response.logos[i].iso_639_1 == 'en') {
+                                logo_path = response.logos[i].file_path;
+                                break;
+                            }
+                        }
+                    }
+                    if (!logo_path) {
+                        logo_path = response.logos[0].file_path;
+                    }
+                }
+
+                if (logo_path) {
+                    var logo_url = Lampa.TMDB.image("/t/p/" + (size === "original" ? "original" : size) + logo_path.replace(".svg", ".png"));
+
+                    title.html('<img style="margin-top:5px; max-height:125px;" src="' + logo_url + '"/>');
+
+                    tagline.remove();
+
+                    if (Lampa.Storage.get("logo_hide_year", true)) {
+                        if (head.length && details.length && details.find(".logo-moved-head").length === 0) {
+                            var head_html = head.html().trim();
+                            if (head_html) {
+                                var separator = details.children().length > 0 ? '<span class="full-start-new__split logo-moved-separator">●</span>' : '';
+                                var moved = '<span class="logo-moved-head" style="margin-left:0.6em;">' + head_html + '</span>';
+                                details.append(separator + moved);
+                                head.remove();
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    });
 })();
